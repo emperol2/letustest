6da17a248a41f606d50e0ea956b26fec79e72b07
Nuttapon <emperol2@gmail.com>
2013-03-08 22:53:42 +0700

    create letustest application

---
+++ letustest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letustest::Application.config.secret_token = 'fe176d4308bba6547b412941ba8623a146cf4d93fe272139a0e6d521c1f18ac1d3928987b432145829331a521fe61416f46136d33851513462129ed7fe49e536'
